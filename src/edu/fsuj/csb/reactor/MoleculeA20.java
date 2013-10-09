bcb86b51b2e189b234115bb8a5ea98cae15b5cd1
Stephan Richter <srichter@srsoftware.de>
2013-10-09 12:00:27 +0200

    removed MoleculeA20 as it can be created by A_Polymer(20)

--- src/edu/fsuj/csb/reactor/MoleculeA20.java
+++
@@ -1,9 +0,0 @@
-package edu.fsuj.csb.reactor;
-
-import edu.fsuj.reactor.molecules.MoleculeA;
-
-public class MoleculeA20 extends MoleculeA {
-  public String formula() {
-	  return "AAAAAAAAAAAAAAAAAAAA";
-  }
-}
